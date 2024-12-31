9f9d75d43b4083256b7f0dfcf9c7ad73c04a97e4
Muhyiddin A M Hayat <muhyiddin@unismuh.ac.id>
2024-12-31 05:34:39 +0000

    Update GenerateRpsInput to make fields nullable for improved flexibility

--- src/rps/dto/generate-rps.input.ts
+++ src/rps/dto/generate-rps.input.ts
@@ -4,28 +4,28 @@ import { InputType, Field } from '@nestjs/graphql';
 export class GenerateRpsInput {
   @Field()
   namaMataKuliah: string;
-  @Field()
-  kodeMataKuliah: string;
-  @Field()
-  rumpunMataKuliah: string;
-  @Field()
-  sks: number;
-  @Field()
-  sksTeori: number;
-  @Field()
-  sksPraktikum: number;
-  @Field()
-  jumlahPertemuan: number;
-  @Field()
-  semester: number;
-  @Field()
-  dosenPengampu: string;
-  @Field()
-  dosenKoordinator: string;
-  @Field()
-  ketuaProgram: string;
-  @Field()
-  bahanKajian: string;
-  @Field()
-  cpl: string;
+  @Field({ nullable: true })
+  kodeMataKuliah?: string;
+  @Field({ nullable: true })
+  rumpunMataKuliah?: string;
+  @Field({ nullable: true })
+  sks?: number;
+  @Field({ nullable: true })
+  sksTeori?: number;
+  @Field({ nullable: true })
+  sksPraktikum?: number;
+  @Field({ nullable: true })
+  jumlahPertemuan?: number;
+  @Field({ nullable: true })
+  semester?: number;
+  @Field({ nullable: true })
+  dosenPengampu?: string;
+  @Field({ nullable: true })
+  dosenKoordinator?: string;
+  @Field({ nullable: true })
+  ketuaProgram?: string;
+  @Field({ nullable: true })
+  bahanKajian?: string;
+  @Field({ nullable: true })
+  cpl?: string;
 }
